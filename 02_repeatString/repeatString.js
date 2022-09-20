94971dace6617d929a8edbd2b579a01664963cc8
Albrecht Waldstein <amusical.drummer@proton.me>
2022-09-20 22:22:03 +0200

    solved 01 and half of 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num > 0){
+    let repeats = string.repeat(num);
+    return  repeats;
+    }else if(num < 0){
+        return "ERROR"
+    }else{
+        return ""
+    }
 };
 
 // Do not edit below this line
