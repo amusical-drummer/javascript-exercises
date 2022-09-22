fb3b1a125b2f4bd0704d266793c616ed00b095c1
Albrecht Waldstein <amusical.drummer@proton.me>
2022-09-22 21:32:49 +0200

    removeFromArray coded to pass first test with only one num argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, num) {
+   let arrBeg = arr.slice(0, num-1);
+   let arrEnd = arr.slice(num);
+   let result = arrBeg.concat(arrEnd);
+   return result 
 };
 
 // Do not edit below this line
